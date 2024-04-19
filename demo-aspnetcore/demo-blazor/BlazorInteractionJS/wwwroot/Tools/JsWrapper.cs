0d594f6fdf75278ba2ffb32b3c0ca13990b910a1
ihab abadi <ihab@utopios.net>
2024-04-19 11:37:57 +0200

    invoke JS avec Blazor

--- demo-aspnetcore/demo-blazor/BlazorInteractionJS/wwwroot/Tools/JsWrapper.cs
+++ demo-aspnetcore/demo-blazor/BlazorInteractionJS/wwwroot/Tools/JsWrapper.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics;
 using System.Runtime.InteropServices.JavaScript;
+using Microsoft.JSInterop;
 
 namespace BlazorInteractionJS.wwwroot.Tools;
 
@@ -6,4 +8,11 @@ public static partial class JsWrapper
 {
     [JSImport("showAlert", "script")]
     public static partial void ShowAlert([JSMarshalAs<JSType.String>] string message);
+
+    
+    [JSInvokable]
+    public static void InvokeCSharpFromJs()
+    {
+        Debug.WriteLine("C# FROM js");
+    }
 }
